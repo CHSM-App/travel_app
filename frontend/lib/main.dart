3e013fc5883debe07b96d858c4517a58a56e30e0
AniketMestry21 <aniketmestry2103@gmail.com>
2026-06-13 18:08:21 +0530

    minor change

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -50,6 +50,7 @@ class MyApp extends StatelessWidget {
           backgroundColor: AppColors.brandHeader,
           foregroundColor: Colors.white,
           elevation: 0,
+          
         ),
       ),
 
